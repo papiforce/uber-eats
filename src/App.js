1d92f5cff62236c4832275935626fb394d7e0d06
Florian R <47738387+FlorianROUVIER@users.noreply.github.com>
2023-11-13 17:44:27 +0100

    Add connection page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { Routes, Route } from 'react-router-dom'
 import "./App.css";
 import Nav from "./components/Nav";
 import SearchFood from "./components/SearchFood";
-
+import Connection from "./pages/Connection"
 
 
 function App() {
   return (
     <div>
       <Nav />
-      <SearchFood />
+      <Routes>
+        <Route
+          path='/'
+          element={<SearchFood />}
+          />
+          <Route
+          path='/login'
+          element={<Connection />}
+          />
+      </Routes>
     </div>
   );
 }
