ce3690388288bfd8dc2aa3f41521a43918bd14c7
Matias gomez <mrmatiasgomez@gmail.com>
2023-11-13 16:02:47 +0100

    First commit

---
+++ src/components/Nav.js
@@ -0,0 +1,166 @@
+import React from "react";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+
+export default function Nav() {
+  const [openNav, setOpenNav] = React.useState(false);
+
+  const [isTransparent, setIsTransparent] = React.useState(true);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setIsTransparent(false);
+    } else {
+      setIsTransparent(true);
+    }
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      ></Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      ></Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      ></Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      ></Typography>
+    </ul>
+  );
+
+  return (
+    <>
+      <Navbar
+        className={`sticky top-0 z-10 max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 ${
+          isTransparent ? "bg-transparent" : "bg-white"
+        }`}
+      >
+        <div className="flex items-center justify-between text-blue-gray-900">
+          <Typography
+            as="a"
+            href="#"
+            className="mr-4 cursor-pointer py-1.5 font-medium no-underline"
+          >
+            EXPRESS <span className="fw-bold"> FOOD</span>
+          </Typography>
+          <div className="flex items-center gap-4">
+            <div className="mr-4 hidden lg:block">{navList}</div>
+            <div className="flex items-center gap-x-1">
+              <Button
+                variant="text"
+                size="sm"
+                className="hidden rounded-full border lg:inline-block"
+              >
+                <FontAwesomeIcon icon={faUser} />
+                <span> Connexion</span>
+              </Button>
+              <Button
+                variant="gradient"
+                size="sm"
+                className="hidden rounded-full lg:inline-block"
+              >
+                <span>Inscription</span>
+              </Button>
+            </div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+        </div>
+        <MobileNav open={openNav}>
+          {navList}
+          <div className="flex items-center gap-x-1 mb-3">
+            <Button
+              fullWidth
+              variant="text"
+              size="sm"
+              className="rounded-full border"
+            >
+              <span>Inscription</span>
+            </Button>
+
+            <Button
+              fullWidth
+              variant="gradient"
+              size="sm"
+              className="rounded-full"
+            >
+              <span>Inscription</span>
+            </Button>
+          </div>
+        </MobileNav>
+      </Navbar>
+    </>
+  );
+}
---
+++ src/components/SearchFood.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function SearchFood() {
+  const backgroundImageUrl =
+    "url(https://www.mashed.com/img/gallery/the-best-new-fast-food-menu-items-weve-tried-in-2023-so-far/l-intro-1682446897.jpg)";
+  return (
+    <>
+      <div
+        className="h-screen bg-cover bg-center flex items-center justify-start"
+        style={{ backgroundImage: backgroundImageUrl }}
+      >
+        <div className="text-white mx-4 w-100">
+          <h2 className="text-xl md:text-4xl font-bold mb-4 text-center">
+            Rechercher un produit
+          </h2>
+          <div className="flex items-center justify-center">
+            <input
+              type="text"
+              placeholder="Rechercher..."
+              className="border border-gray-300 text-dark w-3/5 focus:outline-none rounded-none p-2 mr-2"
+            />
+            <button className="bg-black text-white rounded-none p-2">
+              Rechercher
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
