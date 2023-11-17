6092f734857e0515d3f2d9d47673fff6577253eb
Florian R <dreathxtreme@gmail.com>
2023-11-17 22:03:32 +0100

    Fix history for orders

--- src/pages/OrderListPage.js
+++ src/pages/OrderListPage.js
@@ -7,23 +7,29 @@ const OrderListPage = () => {
   const [orders, setOrders] = useState([]);
 
   const onGetOrderSuccess = (payload) => {
-    setOrders(payload);
+    setOrders(payload.free.concat(payload.pending, payload.finish));
   };
 
   useGetOrders("", onGetOrderSuccess);
 
   return (
-    <Layout>
-      <h2 className="text-center w-100 mt-5">Liste de mes commandes</h2>
+    <div>
+      <Layout>
+        <h2 className="text-center w-100 mt-5">Liste de mes commandes</h2>
 
-      <div className="w-full max-w-6xl mx-auto bg-white border  border-gray-200 mt-5 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <ul className="divide-y">
-          {orders.map((order) => (
-            <OrderCard order={order} key={order._id} />
-          ))}
-        </ul>
-      </div>
-    </Layout>
+        <div className="w-full max-w-6xl mx-auto bg-white border  border-gray-200 mt-5 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <ul className="divide-y mb-0">
+            {orders.length > 0 ? (
+              orders.map((order) => (
+                <OrderCard order={order} key={order._id} />
+              ))
+            ) : (
+              <p className="mx-auto mb-0 max-w-max">Aucune commande</p>
+            )}
+          </ul>
+        </div>
+      </Layout>
+    </div>
   );
 };
 export default OrderListPage;
